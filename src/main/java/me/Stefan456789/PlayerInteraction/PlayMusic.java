609958808770a5874f9ff8b298222800e07a7ab8
Stefan456789 <stefanfarbe@gmail.com>
2021-02-15 16:43:07 +0100

    Fixed Bugs

--- src/main/java/me/Stefan456789/PlayerInteraction/PlayMusic.java
+++ src/main/java/me/Stefan456789/PlayerInteraction/PlayMusic.java
@@ -21,7 +21,7 @@ public class PlayMusic implements Listener {
     @SuppressWarnings("deprecation")
     @EventHandler
     public void playerInteractEvent(PlayerInteractEvent e) {
-        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && e.getClickedBlock().getBlockData().getMaterial().equals(Material.JUKEBOX)) {
+        if ((e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getAction().equals(Action.LEFT_CLICK_BLOCK)) && e.getClickedBlock().getBlockData().getMaterial().equals(Material.JUKEBOX)) {
             Jukebox jukebox = (Jukebox) e.getClickedBlock().getState();
 
             if (jukebox.isPlaying() && jukebox.getRecord().getItemMeta().getCustomModelData() != 0) {
@@ -29,7 +29,7 @@ public class PlayMusic implements Listener {
                 Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "execute as " + e.getPlayer().getName() + " at " + e.getPlayer().getName() + " run stopsound @a[distance=..64] record minecraft:music_disc." + jukebox.getRecord().getItemMeta().getCustomModelData());
 
             }
-            if (!jukebox.isPlaying() && e.getPlayer().getItemInHand().getItemMeta().getCustomModelData() != 0) {
+            if (!jukebox.isPlaying() && e.getPlayer().getItemInHand().getItemMeta().getCustomModelData() != 0 && e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
                 String cmd2 = "execute as " + e.getPlayer().getName() + " at " + e.getPlayer().getName() + " run playsound minecraft:music_disc." + e.getPlayer().getItemInHand().getItemMeta().getCustomModelData() + " record @a[distance=..64]";
                 Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), cmd2);
 
